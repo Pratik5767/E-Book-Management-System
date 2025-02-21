77acd446352330800fbddece82daa48c2d680220
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-02-21 11:56:32 +0530

    .gitignore updated

--- EBookApp/src/main/java/com/db/DBConnect.java
+++ EBookApp/src/main/java/com/db/DBConnect.java
@@ -1,8 +1,11 @@
 package com.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnect {
 
@@ -18,20 +21,24 @@ public class DBConnect {
 
 	public static Connection getConnection() {
 		try {
-			/*
-			 * FileInputStream fis = new FileInputStream(
-			 * "D:\\Pratik\\Servlet and JSP\\EBookApp\\src\\main\\java\\com\\properties\\application.properties"
-			 * ); Properties properties = new Properties(); properties.load(fis);
-			 * 
-			 * connection = DriverManager.getConnection(properties.getProperty("url"),
-			 * properties.getProperty("user"), properties.getProperty("password"));
-			 */
-			connection = DriverManager.getConnection("jdbc:mysql:///ebook", "root", "Pratik@database");
-			
+
+			FileInputStream fis = new FileInputStream(
+					"D:\\Pratik\\git\\EBookRepo\\EBookApp\\src\\main\\java\\properties\\application.properties");
+			Properties properties = new Properties();
+			properties.load(fis);
+
+			connection = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"),
+					properties.getProperty("password"));
+
+			// connection = DriverManager.getConnection("jdbc:mysql:///ebook", "root",
+			// "Pratik@database");
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+
 		if (connection == null)
 			return null;
 		return connection;
